7b4c723b08c664224aed42b5e3d6b75e80efe3a5
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-05 23:38:46 -0500

    Processing response from api request working.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 /* global $ */
-$(document).ready(function () {
+$(document).ready(function() {
 
 });
+var getData = function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://labs.bible.org/api/?passage=random&formatting=plain&type=json', false);
+  xhr.send();
+
+  console.log(xhr.status);
+  console.log(xhr.statusText);
+
+  var str = xhr.responseText;
+  var newStr = str.replace('[', '').replace(']', '');
+
+  var parseStr = JSON.parse(newStr);
+  console.log(parseStr.bookname +  ' ' +  parseStr.chapter + ':' + parseStr.verse);
+  console.log(parseStr.text);
+
+};
+getData();
