2f517010e71a5d04823bc9be0c6f573f434e34c5
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-07 11:04:53 -0500

    Final cleanup.  Project complete.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 /* global $ */
 $(document).ready(function() {
-  console.log('***Document Ready...');
+
   getData();
+
   $('#newQuote').click(function() {
     getData();
   });
-});
 
+});
 function getData() {
-  console.log('***Starting AJAX JSONP...');
+
   $.ajax({
     cache: true,
     type: 'POST',
@@ -23,44 +24,29 @@ function getData() {
     jsonp: 'jsonp',
     jsonpCallback: 'updateHTML'
   });
-}
 
+}
 function updateHTML(res) {
-  console.log('***Starting JSONP callback...');
+
   $('#quote').text(res.quoteText);
   $('#author').text(res.quoteAuthor);
   $('#link').attr('href', res.quoteLink);
 
   var combinedQuote = res.quoteAuthor + ' - ' + res.quoteText;
 
-  console.log(combinedQuote + res.quoteLink);
-  console.log(combinedQuote.length + res.quoteLink.length - 13);
+  if (combinedQuote.length > 140) {
 
-  if (combinedQuote.length + res.quoteLink.length - 13 >= 140) {
-    console.log('>=140');
     $('#tweetDiv').empty();
+
   } else {
-    console.log('<140');
+
     $('#tweetDiv').empty();
-    console.log('***Building link...');
-    var beginA = '<a ';
-    var hrefA = 'href="https://twitter.com/share" ';
-    var classA = 'class="twitter-share-button tweetBtn" ';
-
-    // var dataUrlA = 'data-url="' + res.quoteLink + '" ';
-    var dataUrlA = 'data-url="' + 'http://wrong' + '" ';
-
-    var dataTextA = 'data-text="' + combinedQuote + '" ';
-    var dataCntA = 'data-count="none"';
-    var endA = '>Tweet</a>';
-    var tweetA = beginA + hrefA + classA + dataUrlA + dataTextA + dataCntA + endA;
-
-    console.log(tweetA);
-
+    var tweetA = '<a id="tweet" class="btn btn-default btn-xs btn-success" ';
+    tweetA += 'href="https://twitter.com/intent/tweet?text=' + combinedQuote.replace(/ /g, '+');
+    tweetA += '" target="_blank" class="col-xs-6" role="button">Tweet</a>';
     $('#tweetDiv').append(tweetA);
-    $('#link').attr('href', res.quoteLink);
-
     $.getScript('https://platform.twitter.com/widgets.js');
-    // twttr.widgets.load();
+
   }
+
 }
