06e97616f343aea9815d540faeebb07152efa398
Gilbert Smith <gjsmith3rd@gmail.com>
2015-10-07 08:16:08 -0500

    Prior to switching from twitter button generator to twitter web intent.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* global $ */
 $(document).ready(function() {
+  console.log('***Document Ready...');
   getData();
-
   $('#newQuote').click(function() {
     getData();
   });
-
 });
 
 function getData() {
+  console.log('***Starting AJAX JSONP...');
   $.ajax({
     cache: true,
     type: 'POST',
@@ -23,11 +23,10 @@ function getData() {
     jsonp: 'jsonp',
     jsonpCallback: 'updateHTML'
   });
-
 }
 
 function updateHTML(res) {
-
+  console.log('***Starting JSONP callback...');
   $('#quote').text(res.quoteText);
   $('#author').text(res.quoteAuthor);
   $('#link').attr('href', res.quoteLink);
@@ -38,17 +37,19 @@ function updateHTML(res) {
   console.log(combinedQuote.length + res.quoteLink.length - 13);
 
   if (combinedQuote.length + res.quoteLink.length - 13 >= 140) {
-
     console.log('>=140');
     $('#tweetDiv').empty();
-
   } else {
     console.log('<140');
-
+    $('#tweetDiv').empty();
+    console.log('***Building link...');
     var beginA = '<a ';
     var hrefA = 'href="https://twitter.com/share" ';
     var classA = 'class="twitter-share-button tweetBtn" ';
-    var dataUrlA = 'data-url="' + res.quoteLink + '" ';
+
+    // var dataUrlA = 'data-url="' + res.quoteLink + '" ';
+    var dataUrlA = 'data-url="' + 'http://wrong' + '" ';
+
     var dataTextA = 'data-text="' + combinedQuote + '" ';
     var dataCntA = 'data-count="none"';
     var endA = '>Tweet</a>';
@@ -56,12 +57,10 @@ function updateHTML(res) {
 
     console.log(tweetA);
 
-    $('#tweetDiv').empty();
     $('#tweetDiv').append(tweetA);
     $('#link').attr('href', res.quoteLink);
 
     $.getScript('https://platform.twitter.com/widgets.js');
     // twttr.widgets.load();
   }
-
 }
